41f488e6228d82ca6388a337a4343805029b54be
svetad <sv.dmytr@gmail.com>
2025-12-04 13:29:24 +0200

    Add python test to build

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,12 +10,10 @@ mkdir -p build
 cd build
 
 # Configure step
-cmake -DPYTHON_EXECUTABLE:FILEPATH="$PYTHON" \
-      -DCMAKE_BUILD_TYPE=Release \
+cmake -DCMAKE_BUILD_TYPE=Release \
       -DCHEMICALFUN_BUILD_TESTS=ON \
-      -DCMAKE_INSTALL_PREFIX="$PREFIX" \
-      -DCMAKE_INSTALL_LIBDIR=$PREFIX/lib \
       ..
 # Build step
 make
 make install
+python -c "import chemicalfun"
