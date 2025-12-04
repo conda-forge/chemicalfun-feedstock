653710a3e269f859f9904e9b5a7695b330af2f24
svetad <sv.dmytr@gmail.com>
2025-12-04 19:28:55 +0200

    Revert build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,10 +10,13 @@ mkdir -p build
 cd build
 
 # Configure step
-cmake -DCMAKE_BUILD_TYPE=Release \
+cmake -DPYTHON_EXECUTABLE:FILEPATH="$PYTHON" \
+      -DCMAKE_BUILD_TYPE=Release \
       -DCHEMICALFUN_BUILD_TESTS=ON \
+      -DCMAKE_INSTALL_PREFIX="$PREFIX" \
+      -DCMAKE_INSTALL_LIBDIR=$PREFIX/lib \
       ..
 # Build step
-make  -j${CPU_COUNT}
+make -j${CPU_COUNT}
 make install
 #python -c "import chemicalfun"
